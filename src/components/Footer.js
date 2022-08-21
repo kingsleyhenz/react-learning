79e5d05ca92523e23cebf1188bb88faeb4491404
BLACKHENZ29 <ayehenz29@gmail.com>
2022-08-21 08:23:26 +0100

    just added the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <div className='textbox'>
+      <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut eius minus, adipisci dolorum officia aspernatur quaerat id, aliquid eligendi eos, totam laboriosam alias ipsam repudiandae tempore odit quam necessitatibus similique. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos aliquam quos numquam ipsa, iste nam qui repellat veniam sequi, non dolorem! Laboriosam, nobis. Eius perferendis eaque corporis, et commodi ab! 
+    </p>
+      </div>
+      <div className='footinput'>
+        <input type={'text'} placeholder={'Enter Your Full Name'}/>
+        <input type={'text'} placeholder={'Enter YOur Email Address'}/>
+        <button>Send Email</button>
+      </div>
+    </footer>
   )
 }
 
